84a21b4e5a2c44f993b8b2a3235ffa74a644a31e
Kim Schulze <kimberlykayschulze@gmail.com>
2017-08-22 11:30:43 -0700

    add skeleton HomeController

---
+++ Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantList.Models;
+
+namespace RestaurantList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/restaurants")]
+        public ActionResult Restaurants()
+        {
+
+        }
+
+        [HttpPost("/restaurants")]
+        public ActionResult AddRestaurants()
+        {
+
+        }
+
+        [HttpGet("/restaurants/new")]
+        public ActionResult RestaurantForm()
+        {
+            return View()
+        }
+
+        [HttpGet("/cuisines")]
+        public ActionResult Cuisines()
+        {
+
+        }
+
+        [HttpPost("/cuisines")]
+        public ActionResult AddCuisines()
+        {
+
+        }
+
+        [HttpGet("/cuisines/new")]
+        public ActionResult CuisineForm()
+        {
+            return View()
+        }
+
+
+    }
+
+
+}
