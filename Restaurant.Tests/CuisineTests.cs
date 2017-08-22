c2efd40445a54c1a9e8ce231c7c16ece1699aa76
Kim Schulze <kimberlykayschulze@gmail.com>
2017-08-22 09:42:16 -0700

    Passing 13 tests in Restaurant and Cuisine class.

--- Restaurant.Tests/CuisineTests.cs
+++ Restaurant.Tests/CuisineTests.cs
@@ -34,6 +34,74 @@ namespace RestaurantList.Tests
             Assert.AreEqual(firstCuisine, secondCuisine);
         }
 
+        [TestMethod]
+        public void Save_SavesCuisineToDatabase_CuisineList()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("Italian");
+            testCuisine.Save();
+
+            //Act
+            List<Cuisine> resultList = Cuisine.GetAll();
+            List<Cuisine> testList = new List<Cuisine> {testCuisine};
+
+            //Assert
+            CollectionAssert.AreEqual(testList, resultList);
+        }
+
+        [TestMethod]
+        public void Save_DatabaseAssignsIdToCuisine_Id()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("Italian");
+            testCuisine.Save();
+
+            //Act
+            Cuisine savedCuisine = Cuisine.GetAll()[0];
+
+            int resultId = savedCuisine.GetId();
+            int testId = testCuisine.GetId();
+
+            //Assert
+            Assert.AreEqual(resultId, testId);
+        }
+
+        [TestMethod]
+        public void Find_FindsCuisineInDatabase_Cuisine()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("Italian");
+            testCuisine.Save();
+
+            //Act
+            Cuisine foundCuisine = Cuisine.Find(testCuisine.GetId());
+
+            //Assert
+            Assert.AreEqual(testCuisine, foundCuisine);
+        }
+
+        [TestMethod]
+        public void GetRestaurants_RetrievesAllRestaurantsWithCuisine_RestaurantList()
+        {
+            //Arrange
+            Cuisine testCuisine = new Cuisine("Italian");
+            testCuisine.Save();
+
+            Restaurant firstRestaurant = new Restaurant("Reedville Cafe", testCuisine.GetId(), "1234 Reedville Street", 5, "burgers", 1);
+            firstRestaurant.Save();
+            Restaurant secondRestaurant = new Restaurant("Panera", testCuisine.GetId(), "567 185th Street", 4, "veggie options", 2);
+            secondRestaurant.Save();
+
+            //Act
+            List<Restaurant> testRestaurantList = new List<Restaurant> {firstRestaurant, secondRestaurant};
+            List<Restaurant> resultRestaurantList = testCuisine.GetRestaurants();
+
+            //Assert
+            CollectionAssert.AreEqual(testRestaurantList, resultRestaurantList);
+        }
+
+
+
         public void Dispose()
         {
             Restaurant.DeleteAll();
